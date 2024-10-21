fe2ddf95a3a96d9122d3abd964616f1f8cf88e5e
Ihor Isametov <akirraint@gmail.com>
2024-10-21 10:25:15 -0400

    deserialization issue fixed

--- src/main/java/serializer/CurrentTimeDeserializer.java
+++ src/main/java/serializer/CurrentTimeDeserializer.java
@@ -8,10 +8,12 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 // input time format looks like "Sunday, October 20, 2024 11:30:45 AM +03:00"
 public class CurrentTimeDeserializer extends JsonDeserializer<ZonedDateTime> {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, LLLL dd, yyyy h:mm:ss a VV");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, LLLL dd, yyyy h:mm:ss a VV")
+            .withLocale(Locale.US);
     @Override
     public ZonedDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
         return ZonedDateTime.parse(jsonParser.getText(), formatter);
